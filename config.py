a2deb78d2d94d938cbdf2328b934d68260f804cd
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-08-28 20:40:35 +0300

    [add] COMPETITORS_LIMIT

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ DATA_FOLDER = "data"
 # there is an idea what if appid is > a number below and its price has never changed - game still have not been released
 THRESHOLD_APP_ID = 282370
 MAX_UPDATES_DELTA = 30
+COMPETITORS_LIMIT = 10_000
