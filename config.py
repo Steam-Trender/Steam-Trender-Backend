ee2b154c4be89171cf5d293c316c9e681c2b326d
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-09-02 11:20:04 +0300

    [fix] revert drop on start

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ DATA_FOLDER = "data"
 # there is an idea what if appid is > a number below and its price has never changed - game still have not been released
 THRESHOLD_APP_ID = 282370
 UPDATE_ON_START = False
-DROP_ON_START = True
+DROP_ON_START = False
 UPDATE_DAY = 30
 COMPETITORS_LIMIT = 1_000
