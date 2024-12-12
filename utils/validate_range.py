0be0d646015c597f7072d43b5dc05f916351b2c3
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-12-12 20:31:38 +0300

    [add] validate range util

---
+++ utils/validate_range.py
@@ -0,0 +1,19 @@
+from datetime import date
+from typing import Optional, Tuple, Union
+
+NumericOrDate = Union[float, date]
+
+
+def validate_range(
+    min_value: Optional[NumericOrDate],
+    max_value: Optional[NumericOrDate],
+    min_max_float_value: Optional[NumericOrDate] = 0,
+) -> Tuple[Optional[NumericOrDate], Optional[NumericOrDate]]:
+    if max_value is not None:
+        if isinstance(max_value, float) and max_value < min_max_float_value:
+            max_value = None
+
+    if min_value is not None and max_value is not None:
+        min_value, max_value = min(min_value, max_value), max(min_value, max_value)
+
+    return min_value, max_value
